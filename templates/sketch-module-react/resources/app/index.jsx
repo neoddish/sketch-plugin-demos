7e1f30f43500543b50d43e646d2c622b33f3f442
jiazhewang <neoddish@outlook.com>
2020-03-23 22:49:14 +0800

    templates init

---
+++ templates/sketch-module-react/resources/app/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.less";
+
+function result(s) {
+  console.log("result");
+  console.log(s);
+
+  this.setState({ info: s });
+}
+
+class TemplateReact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      info: ""
+    };
+
+    window.result = result.bind(this);
+  }
+
+  go(msg) {
+    console.log("click btn", msg);
+    window.postMessage("sayhi", msg);
+  }
+
+  render() {
+    const { info } = this.state;
+
+    return (
+      <>
+        <h1>Test</h1>
+        <h2>{info}</h2>
+        <button
+          type="button"
+          onClick={() => {
+            this.go("hey");
+          }}
+        >
+          Click Me!
+        </button>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<TemplateReact />, document.getElementById("root"));
